ed844e899261019408ad55477e3df6dceb206cf0
aafs <afiguerog@gmail.com>
2024-12-12 18:49:05 -0600

    GridSearchBarPlot

---
+++ MachineLearning/Projects/Classification_TabularVectorBorneDiseaseDataSet/Common.py
@@ -0,0 +1,16 @@
+import zipfile 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import KFold, cross_val_score
+import random
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+
